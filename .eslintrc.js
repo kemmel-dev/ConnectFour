7b86382a60eae631d14f7a9d98058e351701a29b
Kamiel de Visser <kamieldevisser@gmail.com>
2022-10-05 18:14:27 +0200

    Init commit: Hello world!

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,10 +8,15 @@ module.exports = {
     '@vue/standard'
   ],
   parserOptions: {
-    parser: '@babel/eslint-parser'
+    parser: '@babel/eslint-parser',
+    ecmaVersion: 2021
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'vue/multi-word-component-names': 0,
+    'vue/no-mutating-props': 0,
+    'no-unused-vars': 0,
+    'no-unreachable': 0
   }
 }
---
+++ src/App31.vue
@@ -0,0 +1,22 @@
+<template>
+  <Header></Header>
+  <Welcome></Welcome>
+</template>
+
+<script>
+import Header from '@/components/Header'
+import Welcome from '@/components/Welcome'
+
+export default {
+  name: 'App31',
+  components: {
+    Header,
+    Welcome
+  }
+
+}
+</script>
+
+<style scoped>
+
+</style>
