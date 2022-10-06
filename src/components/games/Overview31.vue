5dc2885b02074861db5bd0f30816c53e493b6a7a
Kamiel <kamieldevisser@gmail.com>
2022-10-06 19:07:26 +0200

    add template for overview which tests the game class

---
+++ src/components/games/Overview31.vue
@@ -0,0 +1,28 @@
+<template>
+  <h1> overview {{ this.getRandGames() }}</h1>
+</template>
+
+<script>
+
+import { Game } from '../models/game'
+
+export default {
+  name: 'Overview31',
+  methods:
+    {
+      getRandGames () {
+        const games = []
+        let index = 0
+        let pid = 30000
+        while (index < 10) {
+          games[index++] = Game.createSampleGame(pid += Math.random(1, 4))
+        }
+        return games
+      }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
