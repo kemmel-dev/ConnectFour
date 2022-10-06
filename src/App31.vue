9cdfd613a3d3d810178be4181869a8375fc19093
Kamiel <kamieldevisser@gmail.com>
2022-10-06 17:17:54 +0200

    add navbar

--- src/App31.vue
+++ src/App31.vue
@@ -1,22 +1,25 @@
 <template>
   <Header></Header>
+  <NavBar></NavBar>
   <Welcome></Welcome>
 </template>
 
 <script>
 import Header from '@/components/Header'
 import Welcome from '@/components/Welcome'
+import NavBar from '@/components/NavBar'
 
 export default {
   name: 'App31',
   components: {
     Header,
-    Welcome
+    Welcome,
+    NavBar
   }
 
 }
 </script>
 
 <style>
-  @import "src/assets/style.css";
+  @import "@/assets/style.css";
 </style>
