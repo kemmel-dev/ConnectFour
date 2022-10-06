fd2a6ff272093c02bfa5cc3d5cad46e2fa39b387
Kamiel de Visser <kamieldevisser@gmail.com>
2022-10-06 13:40:00 +0200

    [wip] format header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,5 +1,20 @@
 <template>
-  <h1>Header!</h1>
+  <div id="header">
+      <div id="logo">
+        <h1>logo</h1>
+      </div>
+      <div id="title">
+        <div id="title1">
+          <h1>Title1</h1>
+        </div>
+        <div id="title2">
+          <h2>Title2</h2>
+        </div>
+      </div>
+      <div id="subtitle">
+        <h3> Subtitle </h3>
+      </div>
+  </div>
 </template>
 
 <script>
@@ -9,5 +24,36 @@ export default {
 </script>
 
 <style scoped>
+  h1, h2, h3 {
+    color: white;
+  }
 
+  #header {
+    width: 100%;
+    height: 125px;
+  }
+  #header div {
+    display: inline-block;
+  }
+  #header div div {
+    display:inline;
+  }
+  #logo
+  {
+    background: black;
+    width: 10%;
+    height: 100%;
+  }
+  #title
+  {
+    background: red;
+    width: 45%;
+    height: 100%;
+  }
+  #subtitle
+  {
+    background: blue;
+    width: 45%;
+    height: 100%;
+  }
 </style>
