12184e4dbfe29c9c4dd275777d841c377286a246
Kamiel <kamieldevisser@gmail.com>
2022-10-07 12:42:57 +0200

    replace welcome with overview

--- src/App31.vue
+++ src/App31.vue
@@ -1,18 +1,21 @@
 <template>
   <Header></Header>
   <NavBar></NavBar>
-  <Welcome></Welcome>
+  <overview31></overview31>
 </template>
 
 <script>
 import Header from '@/components/Header'
 import Welcome from '@/components/Welcome'
 import NavBar from '@/components/NavBar'
+import Overview31 from '@/components/games/Overview31'
 
 export default {
   name: 'App31',
   components: {
+    Overview31,
     Header,
+    // eslint-disable-next-line vue/no-unused-components
     Welcome,
     NavBar
   }
