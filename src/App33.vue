7682247a024efb7a8c4858e43f42cbef685c00da
Kamiel <kamieldevisser@gmail.com>
2022-10-08 21:23:45 +0200

    add App33.vue

---
+++ src/App33.vue
@@ -0,0 +1,23 @@
+<template>
+  <Header></Header>
+  <NavBar></NavBar>
+  <router-view></router-view>
+</template>
+
+<script>
+import Header from '@/components/Header'
+import NavBar from '@/components/NavBar'
+
+export default {
+  name: 'App31',
+  components: {
+    Header,
+    NavBar
+  }
+
+}
+</script>
+
+<style>
+  @import "@/assets/style.css";
+</style>
